7b93cce738ea8f8d9701bbc133c988f9125911b2
iam-srikar <srikarmarikanti@gmail.com>
2023-12-01 18:26:57 +0530

    Student application

--- app/src/main/java/com/srikar/studentinfo/Splash.kt
+++ app/src/main/java/com/srikar/studentinfo/Splash.kt
@@ -1,11 +1,25 @@
 package com.srikar.studentinfo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.srikar.studentinfo.databinding.ActivitySplashBinding
 
 class Splash : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var intent = Intent(this,MainActivity::class.java)
+
+        Handler().postDelayed(Runnable {
+            startActivity(intent)
+            finish()
+        },1000)
+
     }
 }
